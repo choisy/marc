295d3d6d7a66264698480d939d8cbaccdcef0e0b
Marc Choisy <mchoisy@gmail.com>
2016-11-15 14:22:06 +0700

    Replacing the function to_basic_latin by the function vn2latin

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,12 +1,8 @@
-basic_latin1 <- basic_latin2 <- read.table("basic_latin.txt",as.is=T)
-# First coding of the Unicode:
-tmp <- sub("\\+","\\\\+",basic_latin1[,1])
-basic_latin1[,1] <- paste0("<",tmp,">")
-# Second possible coding of the Unicode:
-basic_latin2[,1] <- tolower(sub("U\\+","\\u",basic_latin2[,2]))
-# Putting the 2 coding together:
-basic_latin <- rbind(basic_latin1,basic_latin2)
+tmp <- read.table("vietnamese_letters.txt")
+latin_letters <- c(rep("A",17),"D",rep("E",11),rep("O",17),rep("U",11),
+                   rep("a",17),"d",rep("e",11),rep("o",17),rep("u",11))
+to_latin <- data.frame(tmp,latin_letters)
 # Save:
-devtools::use_data(basic_latin,internal=T,overwrite=T)
+devtools::use_data(to_latin,internal=T,overwrite=T)
 # Clean:
-rm(basic_latin,basic_latin1,basic_latin2,tmp)
+#rm(tmp,latin_letters,to_latin)
