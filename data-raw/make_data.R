8e82e2b63f91fe48114e7d610e64cfa53e431f08
Marc Choisy <mchoisy@gmail.com>
2016-11-15 14:30:02 +0700

    Adding the "i"s to the Vietnamese letters

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,8 +1,8 @@
 tmp <- read.table("vietnamese_letters.txt")
-latin_letters <- c(rep("A",17),"D",rep("E",11),rep("O",17),rep("U",11),
-                   rep("a",17),"d",rep("e",11),rep("o",17),rep("u",11))
+latin_letters <- c(rep("A",17),"D",rep("E",11),rep("O",17),rep("U",11),rep("I",5),
+                   rep("a",17),"d",rep("e",11),rep("o",17),rep("u",11),rep("i",5))
 to_latin <- data.frame(tmp,latin_letters)
 # Save:
 devtools::use_data(to_latin,internal=T,overwrite=T)
 # Clean:
-#rm(tmp,latin_letters,to_latin)
+rm(tmp,latin_letters,to_latin)
