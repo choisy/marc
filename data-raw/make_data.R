dbf2eb5a35b90cb4b860a7b0b84e77aa12fc9635
Marc Choisy <mchoisy@gmail.com>
2016-11-15 10:12:11 +0700

    Adding another coding of the Unicode to the basic_latin hidden object

--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,5 +1,12 @@
-basic_latin <- read.table("basic_latin.txt",as.is=T)
-tmp <- sub("\\+","\\\\+",basic_latin[,1])
-basic_latin[,1] <- paste0("<",tmp,">")
-devtools::use_data(basic_latin,internal=T)
-rm(basic_latin,tmp)
+basic_latin1 <- basic_latin2 <- read.table("basic_latin.txt",as.is=T)
+# First coding of the Unicode:
+tmp <- sub("\\+","\\\\+",basic_latin1[,1])
+basic_latin1[,1] <- paste0("<",tmp,">")
+# Second possible coding of the Unicode:
+basic_latin2[,1] <- tolower(sub("U\\+","\\u",basic_latin2[,2]))
+# Putting the 2 coding together:
+basic_latin <- rbind(basic_latin1,basic_latin2)
+# Save:
+devtools::use_data(basic_latin,internal=T,overwrite=T)
+# Clean:
+rm(basic_latin,basic_latin1,basic_latin2,tmp)
