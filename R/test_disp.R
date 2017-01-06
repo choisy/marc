f834f77b8cddd7f987dffec147422da7430266aa
Marc Choisy <mchoisy@gmail.com>
2017-01-06 14:59:38 +0700

    Adding the dispersion parameter to the output of test_disp

--- R/test_disp.R
+++ R/test_disp.R
@@ -9,11 +9,15 @@
 #' @examples
 #' m <- glm(Species=="versicolor" ~ Sepal.Width,family=binomial(), data=iris)
 #' test_disp(m)
-#' @source Method suggested in the book "R in action" by Kabacoff (2011).
+#' @source method suggested in the book "R in action" by Kabacoff (2011).
+#' @return returns a named vector of 4 components: dispersion parameter ("phi"),
+#' statistic ("stat"), number of degrees of freedom ("df") and corresponding p
+#' value from the test ("p").
 test_disp <- function(m) {
   m2 <- update(m,family=paste0("quasi",family(m)$family))
-  stat <- summary(m2)$dispersion * m$df.residual
+  phi <- summary(m2)$dispersion
+  stat <- phi * m$df.residual
   df <- m$df.residual
   p <- pchisq(stat,df,lower=stat<1)
-  c(stat=stat,df=df,p=p)
+  c(phi=phi,stat=stat,df=df,p=p)
 }
