d3b13959a16a3064a209f9ac360b28c0b6946d99
Marc Choisy <mchoisy@gmail.com>
2017-04-01 20:11:40 +0700

    Adding body and subject to sendRDS function Also adding the possibility to send with aliases

--- R/sendRDS.R
+++ R/sendRDS.R
@@ -14,12 +14,21 @@
 # Note: you'll need to change the security parameters of your email account
 # Note: you may want to forward all the email of your new account towards another one.
 # Note: you may want to define the export REPLYTO=mchoisy@gmail.com is your .bashrc file.
-sendRDS <- function(object, to) {
+sendRDS <- function(object, to, body, subject) {
   object_name <- deparse(substitute(object))
+  if(missing(body)) body <- paste0("object ", object_name, " sent from R")
+  if(missing(subject)) subject <- paste0("object ", object_name, " sent from R")
+  to <- deparse(substitute(to))
+  if(!grepl("@", to)) {
+    tmp <- get(".addresses_book", envir = .GlobalEnv)[to]
+    if(is.na(tmp)) stop(paste0("'", to, "' is not in addresses book"))
+    else to <- tmp
+  }
   rds_name <- paste0(object_name, ".rds")
   saveRDS(object, rds_name)
-  system(paste0("uuencode ", rds_name, " ", rds_name,
-                " | mail -s 'object ", object_name, " sent from R' ",
-                deparse(substitute(to))))
+#  system(paste0("uuencode ", rds_name, " ", rds_name,
+#                " | mail -s 'object ", object_name, " sent from R' ", to))
+  system(paste0("(echo '", body, "'; uuencode ", rds_name, " ", rds_name,
+                ") | mail -s '", subject, "' ", to))
   invisible(file.remove(rds_name))
 }
