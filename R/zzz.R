b97ce2c2562272b6c04f1db29823d2ef414d2943
Marc Choisy <mchoisy@gmail.com>
2017-04-01 20:11:00 +0700

    Adding zzz.R file

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+.onLoad <- function(libname, pkgname) {
+  path <- paste0(.libPaths(), "/marc_data")
+  file <- paste0(path, "/addresses_book.rda")
+  if(dir.exists(path)) load(file)
+  else {
+    dir.create(path)
+    addresses_book <- NULL
+    save(addresses_book, file = file)
+  }
+  assign(".addresses_book", addresses_book, envir = .GlobalEnv)
+  rm(addresses_book)
+}
