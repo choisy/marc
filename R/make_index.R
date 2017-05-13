94b1c60e73d2a898e1dcbe7b9b7df1abaed408f1
Marc Choisy <mchoisy@gmail.com>
2017-05-13 11:49:07 +0700

    Correcting make_index

--- R/make_index.R
+++ R/make_index.R
@@ -1,5 +1,5 @@
 #' @export
 make_index <- function() {
   system(paste0("sed 5,9d vignettes/",
-                dir("vignettes"), " > ../website/index.Rmd"))
+                dir("vignettes"), " > website/index.Rmd"))
 }
