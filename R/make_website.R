aa459738c88eb8c9a3b2de12683727c4f9403e56
Marc Choisy <mchoisy@gmail.com>
2017-05-15 17:52:53 +0700

    Updating make_website

--- R/make_website.R
+++ R/make_website.R
@@ -1,5 +1,5 @@
 #' @export
-make_website <- function(websitename) {
+make_website <- function(websitename = paste(basename(getwd()), "package")) {
   dir.create("website")
   cat(paste0("name: \"", websitename,
              "\"\nhref: index.html\noutput_dir: \"../docs\""),
