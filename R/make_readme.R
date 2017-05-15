b5cbbdd22dd14231505d2d8afa418a675513c8e6
Marc Choisy <mchoisy@gmail.com>
2017-05-15 17:51:20 +0700

    Correcting make_readme

--- R/make_readme.R
+++ R/make_readme.R
@@ -1,5 +1,6 @@
 #' @export
 make_readme <- function(from = 1, to = 22) {
   cat(paste("#", basename(getwd()), "\n"), file = "README.md")
-  system(paste0("sed ", from, ",", to, "d vignettes/", dir("vignettes"), " >> README.md"))
+#  system(paste0("sed ", from, ",", to, "d vignettes/", dir("vignettes"), " >> README.md"))
+  system(paste0("sed ", from, ",", to, "d vignettes/", grep("\\.R$", dir("vignettes"), value = TRUE), " >> README.md"))
 }
