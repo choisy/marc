35770646b921e93245d86de32a7b39783d8a8e3f
Marc Choisy <mchoisy@gmail.com>
2017-05-15 17:56:53 +0700

    Correcting make_readme and make_index

--- R/make_index.R
+++ R/make_index.R
@@ -2,6 +2,6 @@
 make_index <- function(from = 5, to = 9) {
   system(paste0("sed ",from,",",to,"d vignettes/",
 #                dir("vignettes"),
-                grep("\\.R$", dir("vignettes"), value = TRUE),
+                grep("\\.Rmd$", dir("vignettes"), value = TRUE),
                 " > website/index.Rmd"))
 }
