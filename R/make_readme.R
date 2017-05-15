1070cf771b95bc958363552bd72bc2d86f0e361d
Marc Choisy <mchoisy@gmail.com>
2017-05-15 15:41:55 +0700

    Updating make_readme

--- R/make_readme.R
+++ R/make_readme.R
@@ -1,5 +1,5 @@
 #' @export
-make_readme <- function() {
+make_readme <- function(from = 1, to = 22) {
   cat(paste("#", basename(getwd()), "\n"), file = "README.md")
-  system(paste0("sed 1,22d vignettes/", dir("vignettes"), " >> README.md"))
+  system(paste0("sed ", from, ",", to, "d vignettes/", dir("vignettes"), " >> README.md"))
 }
