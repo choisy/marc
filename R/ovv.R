e249cf8b8ff71b7b18490245524d7bc8ffe473a8
Marc Choisy <mchoisy@gmail.com>
2017-06-13 15:49:27 +0700

    Fixing ovv

--- R/ovv.R
+++ R/ovv.R
@@ -19,7 +19,8 @@ ovv <- function(df, digits = 4, interspace = 3) {
   t %<>% mutate_if(is.numeric, round, digits = digits)
   h %<>% mutate_all(as.character)
   t %<>% mutate_all(as.character)
-  m <- setNames(as.data.frame(matrix(".", interspace, ncol(df))), names(df))
+  m <- setNames(as.data.frame(matrix(".", interspace, ncol(df)),
+                              stringsAsFactors = FALSE), names(df))
   out <- bind_rows(h, m, t)
   out <- cbind(c(hn, rep(".", interspace), tn), out)
   names(out)[1] <- ""
