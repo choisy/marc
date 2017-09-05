f243044978c76ea396d250de8d1869eea924a0ad
Marc Choisy <mchoisy@gmail.com>
2017-09-05 12:22:38 +0700

    Adding function datasets

---
+++ R/datasets.R
@@ -0,0 +1,9 @@
+#' blablabla
+#'
+#' blablabla
+#'
+# returns a character vector containing the names of the datasets in package
+# 'pkg'
+datasets <- function(pkg) {
+  data(package = pkg)$results[, "Item"]
+}
