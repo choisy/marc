e9c3d0d0ccd12ac3298d5e7e6821d6cd0fbf617d
Marc Choisy <mchoisy@gmail.com>
2017-10-18 20:08:47 +0700

    Add the addresses function

--- R/addresses.R
+++ R/addresses.R
@@ -1,11 +1,38 @@
-#' addresses
+#' Manages the addresses book
 #'
-#' sdfsf
+#' This function allows to print the content of the addresses book as well as to
+#' retrieve, define or remove one address.
 #'
-#' fsdfsdf
-#' qsfqsdf
-#'
+#' @param \code{x} an optional unquoted character string.
+#' @return If \code{x} is missing, returns the content of the addresses book. If
+#' \code{x} is a pseudo, it returns the corresponding email addresse. If \code{x}
+#' is a formula with a pseudo on the left hand side of the tild sign and an
+#' email address on the right hand side of it, it assigns this news entry to the
+#' address book.
 #' @export
+#' @author Marc Choisy
+#' @seealso The pseudo of an entry of the addreses book can be used in place of
+#' its corresponding email address to sending object with the \code{send} function.
+#' @examples
+#' # When the addresses book is empty:
+#' addresses()
+#'
+#' # Assigning 3 entries to the addresses book:
+#' addresses(pseudo1 ~ name1@domain1)
+#' addresses(pseudo2 ~ name2@domain2)
+#' addresses(pseudo3 ~ name3@domain3)
+#'
+#' # Printing the content of the addresses book:
+#' addresses()
+#'
+#' # Trying to use a pseudo already present in the addresses book:
+#' addresses(pseudo3 ~ name4@domain4, overwrite = TRUE)
+#'
+#' # Replacing the address of a pseudo with a new address:
+#' addresses(pseudo3 ~ name4@domain4)
+#'
+#' # Retrieving an email address:
+#' addresses(pseudo3)
 addresses <- function(x, overwrite = FALSE) {
   fct <- function(x, i) gsub(" *", "", sub("()", "", x[i]))
   addresses_book <- get(".addresses_book", envir = .GlobalEnv)
