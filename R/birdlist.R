99b19badbaf77f88881bc00b929e84d4d4f99b15
Marc Choisy <marc.choisy@ird.fr>
2018-06-17 18:48:58 +0700

    Adding the birdlist function

---
+++ R/birdlist.R
@@ -0,0 +1,15 @@
+#' Print bird list from eBird report
+#'
+#' The eBird report should be generated from the
+#' "My eBird" > "Summarize My Observations" and then "Download this report".
+#' The report in question should be a tabulation-separated text file named
+#' "eBirdReports.xls".
+#'
+#' @export
+#'
+#' @author Marc Choisy
+#'
+birdlist <- function(file = "~/Desktop/eBirdReports.xls") {
+  ebird <- read.table("~/Desktop/eBirdReports.xls", skip = 12, sep = "\t")
+  cat(paste(gsub("\t.*\t", "", ebird[, 1], perl = T), collapse = "\n"))
+}
